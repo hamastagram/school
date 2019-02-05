bf1f7ac41175e3783f2cb3b29f7e1c7bc128e227
hamastagram <kurdb14@gmail.com>
2019-02-06 02:10:54 +0300

    inti commit

---
+++ script.js
@@ -0,0 +1,61 @@
+class School{
+
+    constructor(){
+        this.teachers = [];
+        this.students = [];
+    }
+
+    addTeacher(teacher){
+        this.teachers.push(teacher);
+    }
+    fireTeacher(index){
+        this.teachers.splice(index, 1);
+    }
+    addStudent(student){
+        this.students.push(student);
+    }
+    fireStudent(index){
+        this.students.splice(index, 1);
+    }
+
+    
+}
+
+class Teacher{
+
+    constructor(FirstName, lastName, major){
+        this.firstName = FirstName;
+        this.lastName = lastName;
+        this.major = major;
+    }
+
+}
+
+class Student{
+
+    constructor(FirstName, lastName, grade){
+        this.firstName = FirstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+}
+
+
+const school = new School();
+
+school.addStudent(new Student("Muhammad", "Najat", "forth grade"));
+
+school.addTeacher(new Teacher("Ahmad", "mutasam", "forth grade"));
+
+
+
+console.log(school.students);
+
+school.fireStudent(0,1);
+
+
+console.log("removed");
+console.log(school.students);
+
+
